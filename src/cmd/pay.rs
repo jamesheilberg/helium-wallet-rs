40980de2175ccf37c8bd9741a2cc0377d5d9e82d
Louis Thiery <thiery.louis@gmail.com>
2021-02-11 14:49:27 -0800

    don't be suspicious

--- src/cmd/pay.rs
+++ src/cmd/pay.rs
@@ -255,9 +255,8 @@ fn calculate_remaining_hnt(
         // if window == 0, simply return the current oracle price
         let oracle_price = if *oracle_window == 0 {
             client.get_oracle_price_current()?
-        }
         // else, use the oracle_window, given in minutes to select max price
-        else {
+        } else {
             let now = SystemTime::now().duration_since(UNIX_EPOCH)?;
             let mut oracle_prices = client.get_oracle_price_predictions()?;
             // filter down predictions that are not in window
